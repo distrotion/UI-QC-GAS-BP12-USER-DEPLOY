4e1c2a6e1ba54ce9557208db8282666087ce372c
Thai Parkerizing <administrator@syn-172-101-034-209.res.spectrum.com>
2025-05-14 14:01:36 +0700

    .

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "96b450205193b5938a54c3de0da97e35",
+const RESOURCES = {"flutter_bootstrap.js": "24a05c4c1a795247e8f5d4bda2ff8634",
 "version.json": "9c2a7ea56516780325f6b81d4f4dd28f",
-"index.html": "cd87ee6198f1beeae00e45ff511e7191",
-"/": "cd87ee6198f1beeae00e45ff511e7191",
-"main.dart.js": "637436882e34641a6aa1b9ec20d6f1bf",
+"index.html": "0dcba8325bccfe71b951ea81531fb89b",
+"/": "0dcba8325bccfe71b951ea81531fb89b",
+"main.dart.js": "58dfbcb56b06ec08f9ba272f179a055a",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
